22989e81a0922c10d129f5f70101503fc98a5c26
Caio Fernandes <caiow.wk@gmail.com>
2021-02-27 21:33:14 -0300

    feat: add favicon to _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,6 +41,7 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
